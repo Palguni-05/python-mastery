a7dfdd082477ade39ee7d91bb711131c9a537135
Palguni <dpalguni@gmail.com>
2026-07-23 06:19:21 +0530

    Add variables practice program

---
+++ 01_basics/variables.py
@@ -0,0 +1,6 @@
+name="Jack"
+age="30"
+print("There once was a man named "+name+",")
+print("he really liked the name "+name+",")
+print("he was "+age+" years old,")
+print("he didnt like being "+age+" years old")
