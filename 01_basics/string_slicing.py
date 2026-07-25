2feb87f74b908d86be93ad64496d9a9f043d9933
Palguni <dpalguni@gmail.com>
2026-07-25 06:04:04 +0530

    learning slicing in python

---
+++ 01_basics/string_slicing.py
@@ -0,0 +1,11 @@
+# String Slicing in Python
+
+name = input("Enter your name: ")
+
+print("Original Name :", name)
+print("First Character :", name[0])
+print("Last Character :", name[-1])
+print("First 3 Characters :", name[:3])
+print("Last 3 Characters :", name[-3:])
+print("Every 2nd Character:", name[::2])
+print("Reversed Name :", name[::-1])
