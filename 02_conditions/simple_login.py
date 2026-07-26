ad460600952fb8a2e41837e18e2a60f8ed5b11c8
Palguni <dpalguni@gmail.com>
2026-07-26 19:14:05 +0530

    program to login with a username and password

---
+++ 02_conditions/simple_login.py
@@ -0,0 +1,7 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "admin" and password == "python123":
+    print("Login Successful!")
+else:
+    print("Invalid Username or Password.")
